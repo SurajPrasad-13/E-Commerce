7ecc79e18b99d7b80d24893edc4696a56e1e415a
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-05-15 10:47:47 +0530

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      App
+      <Navbar/>
       
     </>
   )
