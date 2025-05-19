7aa0532143359732f47ee75fe5d5471c63a2ab1c
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-05-19 15:40:45 +0530

    added Footer Section

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo.webp";
+import instagram_icon from "../../assets/instagram.png";
+import facefook_icon from "../../assets/facebook.png";
+import whatsapp_icon from "../../assets/whatsapp.png";
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-logo">
+        <img src={logo} alt="" height="45px" />
+        <p>Shopify</p>
+      </div>
+
+      <ul className="footer-links">
+        <li>About</li>
+        <li>Product</li>
+        <li>Offices</li>
+        <li>Company</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className="footer-social-icon">
+        <div className="footer-social-container">
+          <img src={instagram_icon} alt="" height="30px" />
+        </div>
+        <div className="footer-social-container">
+          <img src={facefook_icon} alt="" height="30px" />
+        </div>
+        <div className="footer-social-container">
+          <img src={whatsapp_icon} alt="" height="30px" />
+        </div>
+      </div>
+      
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2023 - All Right Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Routes/Layout.jsx
+++ src/Routes/Layout.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
-import Footer from '../Footer'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer/Footer'
 
 const Layout = () => {
   return (
