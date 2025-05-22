bfdbf30d6182a01a6d80d3ed15cfed1bdef3764f
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-05-22 22:36:03 +0530

    added Login SignUp page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import "../CSS/LoginSignup.css";
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
 
-export default LoginSignup
+        <p className="loginsignup-login">
+          Already have an Account ? <span> Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,12 +1,11 @@
 import React, { useContext, useState } from "react";
 import "../CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
-
 import dropDown_icon from "../assets/dropdown_icon.png";
 import Item from "../Components/Items/Item";
-
+import all_product from "../assets/all_product";
 const ShopCategory = (props) => {
-  const { all_product } = useContext(ShopContext);
+  const {all_product} = useContext(ShopContext)
   return (
     <div className="shop-category">
       <img className="shopcategory-banner" src={props.banner} alt="" />
@@ -16,16 +15,16 @@ const ShopCategory = (props) => {
           <span>Showing 1-12</span> out of 36 products
         </p>
         <div className="shopcategory-sort">
-          Sort by <img src={dropDown_icon} alt="" height='20px' />
+          Sort by <img src={dropDown_icon} alt="" height="20px" />
         </div>
       </div>
-
+      
       <div className="shopcategory-products">
-        {all_product.map((item, i) => {
-          if (props.category === item.category) {
+        {all_product.map((item, id) => {
+          if (props.category == item.category) {
             return (
               <Item
-                key={i}
+                key={id}
                 id={item.id}
                 name={item.name}
                 image={item.image}
@@ -33,14 +32,12 @@ const ShopCategory = (props) => {
                 old_price={item.old_price}
               />
             );
-          }else{
-            return null
+          } else {
+            return null;
           }
         })}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More..
-      </div>
+      <div className="shopcategory-loadmore">Explore More..</div>
     </div>
   );
 };
