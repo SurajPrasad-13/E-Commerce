4bc995bf4e2a5b6997654d79a42a6b7c1cc12064
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-08-07 15:44:13 +0530

    updated the Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import "./Hero.css";
 import hand_icon from "../../assets/hand_icon.png";
 import arrow_icon from "../../assets/arrow_icon.png";
-import men from "../../assets/p8.webp";
+import hero_girl from '../../assets/p8.webp'
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-left">
-        <h2>Best Deals ! Best Prices !</h2>
-        <div className="hero-hand-icon">
-          <p>New</p>
-          <img src={hand_icon} height="60px" alt="" />
+    <div className=" h-[75vh] sm:h-[90vh] md:h-screen flex justify-between bg-gradient-to-b from-green-300 to-white">
+      <div className="flex flex-col justify-center pl-20">
+        <h2 className="text-xl md:text-2xl font-semibold">Best Deals! Best Prices!</h2>
+        <div className="flex items-center gap-5">
+          <p className="text-2xl sm:text-4xl md:text-5xl font-bold">New</p>
+          <img src={hand_icon} className="h-12" />
         </div>
-        <p>Collections</p>
-        <p>For EveryOne</p>
-        <div className="hero-latest-btn">
+        <p className="text-2xl sm:text-4xl md:text-5xl font-bold">Colleciton</p>
+        <p className="text-2xl sm:text-4xl md:text-5xl font-bold">for everyOne</p>
+
+        <div className="flex items-center justify-center gap-1 w-40 h-8 sm:w-50 sm:h-10 md:w-60 md:h-12 rounded-full mt-7 bg-[#ff4141] text-white text-[14px] sm:text-[16px] md:text-[18px] font-medium ">
           <div>Latest Collections</div>
-          <img src={arrow_icon} alt="" height="30px" />
+          <img src={arrow_icon} alt="" className=" h-5 sm:h-6 md:h-8" />
         </div>
       </div>
 
-      <div className="hero-right">
-        <img src={men} height="450px" alt="" />
+      <div className="flex items-center justify-center">
+        <img src={hero_girl} alt=""  />
       </div>
     </div>
   );
